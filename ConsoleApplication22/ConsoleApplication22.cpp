2c2587cb5c9ed07ac73332a1923ae2a4a8354fb1
Max <pod9orny.max@ya.ru>
2023-11-24 00:39:49 +0300

    DA

--- ConsoleApplication22/ConsoleApplication22.cpp
+++ ConsoleApplication22/ConsoleApplication22.cpp
@@ -4,11 +4,11 @@ using namespace std;
 
 int main() {
     float Tf, Tc;
-    // Из Фаренгейта в шкалу Цельсия.
+    // Из Фаренгейта в Цельсия.
     cout << "Tf = "; cin >> Tf;
     Tc = (Tf - 32) * 5 / 9;
     cout << "Tc = " << Tc << endl;
-    // Из Цельсия в шкалу Фаренгейт.
+    // Из Цельсия в Фаренгейт.
     cout << "Tc = "; cin >> Tc;
     Tf = Tc * 9 / 5 + 32;
     cout << "Tf = " << Tf << endl;
