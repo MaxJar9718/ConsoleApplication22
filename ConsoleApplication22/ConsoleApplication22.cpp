4e95a79ba09c9e2c97cf9d8becb5b9bdd924b41c
Tatiana <Tatiana@DESKTOP-JK80VVF>
2023-11-24 00:26:15 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication22/ConsoleApplication22.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+    float Tf, Tc;
+    // Из Фаренгейта в шкалу Цельсия 
+    cout << "Tf = "; cin >> Tf;
+    Tc = (Tf - 32) * 5 / 9;
+    cout << "Tc = " << Tc << endl;
+    // Из Цельсия в шкалу Фаренгейта
+    cout << "Tc = "; cin >> Tc;
+    Tf = Tc * 9 / 5 + 32;
+    cout << "Tf = " << Tf << endl;
+    return 0;
+}
