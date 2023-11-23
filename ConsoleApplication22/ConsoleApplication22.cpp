ea173d2c36c5bb9e700d7b63b23b569ac5bf10f5
Max <pod9orny.max@ya.ru>
2023-11-24 00:36:36 +0300

    DADADA

--- ConsoleApplication22/ConsoleApplication22.cpp
+++ ConsoleApplication22/ConsoleApplication22.cpp
@@ -4,11 +4,11 @@ using namespace std;
 
 int main() {
     float Tf, Tc;
-    // Из Фаренгейта в шкалу Цельсия 
+    // Из Фаренгейта в шкалу Цельсия.
     cout << "Tf = "; cin >> Tf;
     Tc = (Tf - 32) * 5 / 9;
     cout << "Tc = " << Tc << endl;
-    // Из Цельсия в шкалу Фаренгейта
+    // Из Цельсия в шкалу Фаренгейт.
     cout << "Tc = "; cin >> Tc;
     Tf = Tc * 9 / 5 + 32;
     cout << "Tf = " << Tf << endl;
